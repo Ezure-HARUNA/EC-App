b0ac4d84d1a2f9f441145e168c3ef9b9e6c81ad5
Ezure-HARUNA <haruna.e@80and.co>
2021-05-16 19:01:45 +0900

    style:TextFieldのUIKit追加

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyD2TX5T2JfClE9yF7cQHoOWLyzCZ3WtliM",
+    authDomain: "ec-app-6bf12.firebaseapp.com",
+    projectId: "ec-app-6bf12",
+    storageBucket: "ec-app-6bf12.appspot.com",
+    messagingSenderId: "503285115300",
+    appId: "1:503285115300:web:6e2deb827aca106b037931",
+    measurementId: "G-5YVJ8R95VM"
+  };
