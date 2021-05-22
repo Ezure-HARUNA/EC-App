f9232cb7b7a692324fc0b673da52de4da339919f
Ezure-HARUNA <haruna.e@80and.co>
2021-05-22 18:10:37 +0900

    ReduxとReactを接続する(Store)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from "react-redux"
+import {signInAction} from "./reducks/users/actions"
 
 const App = () => {
   const dispatch = useDispatch()
@@ -9,6 +10,9 @@ const App = () => {
   return (
     <div>
       React app
+      <button onClick={() => dispatch(signInAction({uid: "00001", username: "torahack"}))}>
+        Sign In
+      </button>
     </div>
   )
 }
